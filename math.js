63ab9438783b67021819488e1afc3e88af1f5038
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-18 20:11:42 +0530

    Create math.js

---
+++ math.js
@@ -0,0 +1,26 @@
+let num = 0;
+console.log(Math.sign(num));
+
+let num1 = -50;
+console.log(Math.sign(num1));
+
+let num2 = 150;
+console.log(Math.sign(num2));
+
+let num3 = NaN;
+console.log(Math.sign(NaN));
+
+let num4 = 'string';
+console.log(Math.sign(num4));
+
+let num5 = 3.75;
+console.log(Math.trunc(num5));
+
+let num6 = 5.66;
+console.log(Math.floor(num6));
+
+let num7 = -4.86;
+console.log(Math.floor(num7));
+
+let num8 = -4.86;
+console.log(Math.trunc(num8));
